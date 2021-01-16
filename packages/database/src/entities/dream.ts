3f6ce19afe73b1e9560613c4501963de2c6ba82a
Margaret Morgan <margonaut@users.noreply.github.com>
2021-01-16 13:13:49 -0500

    Add Dream entity and migration

---
+++ packages/database/src/entities/dream.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  BaseEntity,
+} from 'typeorm'
+
+@Entity({
+  name: 'dreams',
+})
+export class Collection extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at!: Date
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at!: Date
+
+  @Column({ nullable: true })
+  name?: string
+
+  @Column({ type: 'text', nullable: true })
+  body?: string
+}
